86fb63394cb00aa636ac73a4228192aee2719d01
JojoFR1 <JojoFR1@users.noreply.github.com>
2024-11-23 21:49:22 +0100

    GTB: Game Settings

--- src/joseta/commands/GameCommand.java
+++ src/joseta/commands/GameCommand.java
@@ -3,16 +3,62 @@ package joseta.commands;
 import joseta.commands.games.*;
 
 import net.dv8tion.jda.api.events.interaction.command.*;
+import net.dv8tion.jda.api.events.interaction.component.*;
 import net.dv8tion.jda.api.hooks.*;
 
 public class GameCommand extends ListenerAdapter {
+    private GuessTheBlock gtbGame = null;
 
     @Override
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
         if (!event.getName().equals("play")) return;
 
         switch (event.getOption("game").getAsString()) {
-            case "gtb" -> GuessTheBlock.play(event);
+            case "gtb" -> playGtb(event);
         }
+    }
+
+    @Override
+    public void onButtonInteraction(ButtonInteractionEvent event) {
+        if (event.getComponentId().equals("gtb-play")) {
+            event.reply("Joueeeeee").queue();
+        }
+        if (event.getComponentId().equals("gtb-cancel")) {
+            if (!event.getUser().getId().equals(gtbGame.launcherId)) {
+                event.reply("Tu n'a pas lancer le jeu, donc tu ne peux pas annuler !")
+                    .setEphemeral(true).queue();
+                return;
+            }
+
+            gtbGame = null;
+            event.getMessage().delete().queue();
+            event.getChannel().sendMessage("Le jeu a été annulé !").queue();
+        }
+    }
+
+    @Override
+    public void onStringSelectInteraction(StringSelectInteractionEvent event) {
+        String guildName = event.getGuild().getName();
+        String guildIcon = event.getGuild().getIconUrl();
+        if (event.getComponentId().startsWith("gtb-")) {
+            if (event.getComponentId().equals("gtb-mode")) {
+                gtbGame.setMode(event.getValues().get(0));
+            }
+            if (event.getComponentId().equals("gtb-difficulty")) {
+                gtbGame.setDifficulty(event.getValues().get(0));
+            }
+
+            event.editMessageEmbeds(gtbGame.config(guildName, guildIcon)).queue();
+        }
+    }
+
+    private void playGtb(SlashCommandInteractionEvent event) {
+        if (gtbGame != null) {
+            event.reply("Un jeu est déjà en cours !").setEphemeral(true).queue();
+            return;
+        }
+
+        gtbGame = new GuessTheBlock(event.getUser().getId());
+        gtbGame.play(event);
     }
 }
--- src/joseta/commands/games/GuessTheBlock.java
+++ src/joseta/commands/games/GuessTheBlock.java
@@ -1,10 +1,73 @@
 package joseta.commands.games;
 
+import net.dv8tion.jda.api.*;
+import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.interaction.command.*;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import net.dv8tion.jda.api.interactions.components.selections.*;
+
+import java.awt.*;
+import java.time.*;
 
 public class GuessTheBlock {
-    
-    public static void play(SlashCommandInteractionEvent event) {
-        event.reply("Saluuuuuuut").queue();
+    public final String launcherId;
+    public String difficulty = "gtb-dmedium";
+    public String mode = "gtb-mall";
+
+    public GuessTheBlock(String launcherId) {
+        this.launcherId = launcherId;
+    } 
+
+    public void play(SlashCommandInteractionEvent event) {
+        String guildName = event.getGuild().getName();
+        String guildIcon = event.getGuild().getIconUrl();
+
+        event.replyEmbeds(config(guildName, guildIcon))
+            .addActionRow(StringSelectMenu.create("gtb-mode")
+                .addOptions(
+                    SelectOption.of("Serpulo", "gtb-mserpulo"),
+                    SelectOption.of("Erekir", "gtb-merekir"),
+                    SelectOption.of("Toutes les Planètes", "gtb-mall")
+                        .withDefault(true)
+                ).build())
+            .addActionRow(StringSelectMenu.create("gtb-difficulty")
+                .addOptions(
+                    SelectOption.of("Facile", "gtb-deasy"),
+                    SelectOption.of("Normal", "gtb-dmedium")
+                        .withDefault(true),
+                    SelectOption.of("Difficile", "gtb-dhard")
+                ).build()
+            ).addActionRow(
+                Button.success("gtb-play", "Jouer"),
+                Button.danger("gtb-cancel", "Annuler")
+            )
+        .queue();
+    }
+
+    public MessageEmbed config(String guildName, String guildIcon) {
+        String difficultyText = difficulty.equals("gtb-deasy") ? "Facile"
+                                : difficulty.equals("gtb-dmedium") ? "Normal"
+                                : "Difficile";
+        String modeText = mode.equals("gtb-mserpulo") ? "Serpulo"
+                            : mode.equals("gtb-merekir") ? "Erekir"
+                            : "Toutes les Planètes";
+
+        EmbedBuilder difficultyEmbed = new EmbedBuilder()
+            .setTitle("Paramètres du jeu")
+            .setColor(Color.CYAN)
+            .setFooter("Guess the Block - " + guildName, guildIcon)
+            .setTimestamp(Instant.now());
+        
+        difficultyEmbed.setDescription("Mode: " + modeText + "\nDifficulté: " + difficultyText);
+        
+        return difficultyEmbed.build();
+    }
+
+    public void setDifficulty(String newDifficulty) {
+        difficulty = newDifficulty;
+    }
+
+    public void setMode(String newMode) {
+        mode = newMode;
     }
 }
