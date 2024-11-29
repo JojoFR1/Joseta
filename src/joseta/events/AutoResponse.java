ca11216186a6185710224ced98f8ce84c20b44c4
JojoFR1 <JojoFR1@users.noreply.github.com>
2024-11-29 17:21:23 +0100

    AutoResponse, small things

--- src/joseta/events/AutoResponse.java
+++ src/joseta/events/AutoResponse.java
@@ -9,14 +9,15 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.*;
 
 public class AutoResponse extends ListenerAdapter {
-    private static Pattern patternQuestion = Pattern.compile(
+    private static final Pattern patternQuestion = Pattern.compile(
         "(?:\\b|[.,?!;:])(?:com*[ea]nt?|pos*ible|m(?:oyen|ani[èeé]re)|fa[cç]on)(?:\\b|[.,?!;:])",
         Pattern.CASE_INSENSITIVE|Pattern.CANON_EQ
     );
-    private static Pattern patternMulti = Pattern.compile(
+    private static final Pattern patternMulti = Pattern.compile(
         "(?:\\b|[.,?!;:])(?:multi[ -]?(?:joueu?r|playeu?r)?|co*p(?:eration|[ea]?ins?)?|amis?|pot[oe]s?|(?:[aà] (?:deux|[2-9]|[1-9]+|plu?si?e?u?rs?)))(?:\\b|[.,?!;:])",
         Pattern.CASE_INSENSITIVE|Pattern.CANON_EQ
     );
+    private final String message = "<:doyouknowtheway:1241824114952372344> Vous voulez héberger votre partie pour jouer avec des amis ?\nVous trouverez plus d'informations ici : <https://zetamap.fr/mindustry_hosting/>";
     
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
@@ -25,7 +26,7 @@ public class AutoResponse extends ListenerAdapter {
         
         if (patternQuestion.matcher(text).find() && patternMulti.matcher(text).find()) {
             Vars.logger.debug("Multiplayer regex match.");
-            msg.reply(getMessage()).queue();
+            msg.reply(message + "\n*Ceci est une réponse automatique possiblement hors-sujet.*").queue();
         }
     }
 
@@ -33,10 +34,6 @@ public class AutoResponse extends ListenerAdapter {
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
         if (!event.getName().equals("multi")) return;
 
-        event.reply(getMessage()).queue();
-    }
-
-    private String getMessage() {
-        return "<:doyouknowtheway:1241824114952372344> Vous voulez héberger votre partie pour jouer avec des amis ?\nVous trouverez plus d'informations ici : <https://zetamap.fr/mindustry_hosting/>\n*Ceci est une réponse automatique possiblement hors-sujet.*";
+        event.reply(message).queue();
     }
 }
