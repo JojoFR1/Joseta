f6d5eebc663694e58696b95fe442926a43b2f4f6
JojoFR1 <JojoFR1@users.noreply.github.com>
2025-03-15 13:18:59 +0100

    Refactor LogSystem: small cleanup

--- src/joseta/events/LogSystem.java
+++ src/joseta/events/LogSystem.java
@@ -8,54 +8,49 @@ import net.dv8tion.jda.api.audit.*;
 import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.*;
 import net.dv8tion.jda.api.events.channel.*;
+import net.dv8tion.jda.api.events.channel.update.*;
 import net.dv8tion.jda.api.hooks.*;
 
 import java.awt.*;
-import java.lang.reflect.*;
 import java.time.*;
 
 public class LogSystem extends ListenerAdapter {
 
-    private <T extends GenericEvent> void sendLog(T event, Guild guild) {
+    @Override
+    public void onGenericEvent(GenericEvent event) {
         EventType eventType = EventType.getFromEventObject(event);
         if (eventType == EventType.NONE) {
             JosetaBot.logger.warn("Unknown event type: " + event);
             return;
         }
 
-        guild.retrieveAuditLogs()
-            .type(eventType.logType)
-            .limit(1)
-            .queue(
-                success -> {
-                    if (success.isEmpty()) return;
-                    
-                    Vars.testChannel.sendMessageEmbeds(eventType.getEmbed(guild, event)).queue();
-                },
-                failure -> {
-                    JosetaBot.logger.error("Error while logging.", failure);
-                }
-            );
+        Vars.testChannel.sendMessageEmbeds(eventType.getEmbed(event)).queue();
     }
 
     public enum EventType {
-        NONE(null, null, (guild, event) -> new EmbedBuilder().setDescription("Error").setFooter(guild.getName(), guild.getIconUrl()).setTimestamp(Instant.now()).build()),
-        CHANNEL_CREATE(ChannelCreateEvent.class, ActionType.CHANNEL_CREATE, (guild, event) -> new EmbedBuilder().setTitle("Salon créé").setDescription("desc").setColor(Color.GREEN).setFooter(guild.getName(), guild.getIconUrl()).setTimestamp(Instant.now()).build()),
-        CHANNEL_DELETE(ChannelDeleteEvent.class, ActionType.CHANNEL_DELETE, (guild, event) -> new EmbedBuilder().setTitle("Salon supprimé").setDescription("desc").setColor(Color.RED).setFooter(guild.getName(), guild.getIconUrl()).setTimestamp(Instant.now()).build());
+        NONE                    (null, null, event -> new EmbedBuilder().setDescription("Error").setTimestamp(Instant.now()).build()),
+        CHANNEL_CREATE          (ChannelCreateEvent.class,          ActionType.CHANNEL_CREATE, event -> Vars.getDefaultEmbed(Color.GREEN, event.getGuild()).setTitle("Salon créé").setDescription("Salon créé: " + event.getChannel().getAsMention()).build()),
+        CHANNEL_DELETE          (ChannelDeleteEvent.class,          ActionType.CHANNEL_DELETE, event -> Vars.getDefaultEmbed(Color.RED, event.getGuild()).setTitle("Salon supprimé").setDescription("desc").build()),
+        CHANNEL_UPDATE_TOPIC    (ChannelUpdateTopicEvent.class,     ActionType.CHANNEL_UPDATE, event -> Vars.getDefaultEmbed(Color.YELLOW, event.getGuild()).setTitle("Salon mis a jour (Topic)").setDescription(event.getOldValue() + " en " + event.getNewValue()).build()),
+        CHANNEL_UPDATE_SLOWMODE (ChannelUpdateSlowmodeEvent.class,  ActionType.CHANNEL_UPDATE, event -> Vars.getDefaultEmbed(Color.YELLOW, event.getGuild()).setTitle("Salon mis a jour (Slowmode)").setDescription(event.getOldValue() + " en " + event.getNewValue()).build()),
+        CHANNEL_UPDATE_NAME     (ChannelUpdateNameEvent.class,      ActionType.CHANNEL_UPDATE, event -> Vars.getDefaultEmbed(Color.YELLOW, event.getGuild()).setTitle("Salon renommé").setDescription(event.getOldValue() + " en " + event.getNewValue()).build()),
+        CHANNEL_UPDATE_NSFW     (ChannelUpdateNSFWEvent.class,      ActionType.CHANNEL_UPDATE, event -> Vars.getDefaultEmbed(Color.YELLOW, event.getGuild()).setTitle("Salon mis a jour (NSFW)").setDescription(event.getOldValue() + " en " + event.getNewValue()).build()),
+        // CHANNEL_UPDATE(ChannelDeleteEvent.class, ActionType.CHANNEL_UPDATE, (guild, event) -> Vars.getDefaultEmbed(Color.YELLOW, event.getGuild()).setTitle("Salon supprimé").setDescription("desc").setColor(Color.RED).setFooter(guild.getName(), guild.getIconUrl()).setTimestamp(Instant.now()).build());
+        ;
 
         public final Class<? extends GenericEvent> eventClass;
-        public final Func2<Guild, ? extends GenericEvent, MessageEmbed> embed;
         public final ActionType logType;
-
-        private <T extends GenericEvent> EventType(Class<T> eventClass, ActionType type, Func2<Guild, T, MessageEmbed> embed) {
+        public final Func<? extends GenericEvent, MessageEmbed> embed;
+        
+        private <T extends GenericEvent> EventType(Class<T> eventClass, ActionType type, Func<T, MessageEmbed> embed) {
             this.eventClass = eventClass;
-            this.embed = embed;
             this.logType = type;
+            this.embed = embed;
         }
         
         @SuppressWarnings("unchecked")
-        public <T extends GenericEvent> MessageEmbed getEmbed(Guild guild, T event) {
-            if (eventClass.isInstance(event)) return ((Func2<Guild, T, MessageEmbed>) embed).get(guild, event);
+        public <T extends GenericEvent> MessageEmbed getEmbed(T event) {
+            if (eventClass.isInstance(event)) return ((Func<T, MessageEmbed>) embed).get(event);
             return new EmbedBuilder().setDescription("Error: Event type mismatch").build();
         }
 
@@ -65,31 +60,5 @@ public class LogSystem extends ListenerAdapter {
             }
             return EventType.NONE;
         }
-    }
-
-    /**
-     * Gets the guild from an event if possible
-     * @param event The event to get the guild from
-     * @return The guild, or null if the event doesn't have a guild
-     */
-    private <T extends GenericEvent> Guild getGuildFromEvent(T event) {
-        try {
-            Method getGuildMethod = event.getClass().getMethod("getGuild");
-            return (Guild) getGuildMethod.invoke(event);
-        } catch (Exception e) {
-            // Method doesn't exist or can't be called
-            return null;
-        }
-    }
-
-    @Override
-    public void onGenericEvent(GenericEvent event) {
-        Guild guild = getGuildFromEvent(event);
-        if (guild == null) {
-            JosetaBot.logger.warn("Event doesn't have a guild: " + event);
-            return;
-        }
-
-        sendLog(event, guild);
     }
 }
