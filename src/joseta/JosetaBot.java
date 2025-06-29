76337a7475b3e6117be0403c8383e0da43101e0c
Jojo <110781915+JojoFR1@users.noreply.github.com>
2025-06-29 20:01:10 +0000

    Experimentation on switching to Hibernate & JPA

--- src/joseta/JosetaBot.java
+++ src/joseta/JosetaBot.java
@@ -20,6 +20,9 @@ import net.dv8tion.jda.api.interactions.commands.build.*;
 import net.dv8tion.jda.api.requests.*;
 import net.dv8tion.jda.api.utils.*;
 
+import org.hibernate.*;
+import org.hibernate.boot.registry.*;
+import org.hibernate.cfg.*;
 import org.slf4j.*;
 
 import java.sql.*;
@@ -27,6 +30,7 @@ import java.util.concurrent.*;
 
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
+import jakarta.persistence.*;
 
 public class JosetaBot {
     private static JDA bot;
@@ -56,6 +60,9 @@ public class JosetaBot {
         registerShutdown();
         preLoad(args);
 
+        dbTets();
+        System.exit(0);
+        
         Databases.getInstance(); // Should do the first initialization.        
         
         bot = JDABuilder.createDefault(Vars.token)
@@ -95,6 +102,61 @@ public class JosetaBot {
                 MarkovMessagesDatabaseHelper.populateNewGuild(guild);
             }
         }
+    }
+
+    public static void dbTets() {
+        Configuration configuration = new Configuration();
+        
+        configuration.addAnnotatedClass(TestTable.class);
+        configuration.setProperty("hibernate.connection.driver_class", "org.sqlite.JDBC");
+        configuration.setProperty("hibernate.connection.url", "jdbc:sqlite:resources/test.db");
+        configuration.setProperty("hibernate.dialect", "org.hibernate.community.dialect.SQLiteDialect");
+        configuration.setProperty("hibernate.hbm2ddl.auto", "update");
+        configuration.setProperty("hibernate.show_sql", "true");
+        
+        StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+        SessionFactory sessionFactory = configuration.buildSessionFactory(builder.build());
+
+        Session s = sessionFactory.openSession();
+        Transaction tx = s.beginTransaction();
+        s.persist(new TestTable(9, "Test", "This is a test table."));
+        
+        tx.commit();
+    }
+
+    @Entity
+    public static class TestTable {
+        @Id
+        private long id;
+        private String name;
+        private String description;
+
+        public TestTable() {}
+        public TestTable(long id, String name, String description) {
+            this.id = id;
+            this.name = name;
+            this.description = description;
+        }
+        
+        public long getId() {
+            return id;
+        }
+        public void setId(long id) {
+            this.id = id;
+        }
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+        public String getDescription() {
+            return description;
+        }
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
     }
 
     private static void preLoad(String args[]) {
