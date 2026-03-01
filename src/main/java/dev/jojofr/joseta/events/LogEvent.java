8455a56d04387fce1f199caa447eddb9c616636a
Jojo <110781915+JojoFR1@users.noreply.github.com>
2026-03-01 16:28:04 +0100

    Use embed for logging

--- src/main/java/dev/jojofr/joseta/events/LogEvent.java
+++ src/main/java/dev/jojofr/joseta/events/LogEvent.java
@@ -7,28 +7,42 @@ import dev.jojofr.joseta.database.entities.ConfigurationEntity;
 import dev.jojofr.joseta.database.entities.MessageEntity;
 import dev.jojofr.joseta.generated.EventType;
 import dev.jojofr.joseta.utils.BotCache;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.message.MessageUpdateEvent;
+
+import java.awt.*;
+import java.time.Instant;
 
 @EventModule
 public class LogEvent {
     
-    //TODO Need some kind of priority system to make sure this runs before the MessageDatabase update
-    @EventHandler(type = EventType.MESSAGE_UPDATE)
+    @EventHandler(type = EventType.MESSAGE_UPDATE, priority = EventHandler.EventPriority.HIGH)
     public void messageUpdateEvent(MessageUpdateEvent event) {
+        if (event.getAuthor().isBot() || event.getAuthor().isSystem()) return;
+        
         ConfigurationEntity configuration = BotCache.getGuildConfiguration(event.getGuild().getIdLong());
         if (configuration == null || !configuration.moderationLogsEnabled) return;
         
         TextChannel channel;
         if (configuration.moderationLogsChannelId == null || (channel = event.getGuild().getTextChannelById(configuration.moderationLogsChannelId)) == null) return;
         
+        MessageEntity oldMessage = Database.get(MessageEntity.class, event.getMessageIdLong());
+        if (oldMessage == null) return; // Message not found in database, cannot log
+        
+        MessageEmbed embed = new EmbedBuilder()
+            .setColor(Color.ORANGE)
+            .setAuthor(event.getAuthor().getEffectiveName(), null, event.getAuthor().getEffectiveAvatarUrl())
+            .setDescription("**Message modifié: " + event.getMessage().getJumpUrl() + "**\n\n" +
+                "**Avant**\n```" + oldMessage.content + "```\n" +
+                "**Après**\n```" + event.getMessage().getContentRaw() + "```")
+            .setFooter(event.getGuild().getName(), event.getGuild().getIconUrl())
+            .setTimestamp(Instant.now())
+            .build();
+        
         // Old content is retrieved from database before it gets updated
-        channel.sendMessage("**Message édité**\n" +
-            "Auteur : "+ event.getAuthor().getAsTag() + " ("+ event.getAuthor().getId() +")\n" +
-            "Salon : "+ event.getChannel().getName() +" ("+ event.getChannel().getId() +")\n" +
-            "Ancien message : "+ Database.get(MessageEntity.class, event.getMessageIdLong()).content + "\n" +
-            "Nouveau message : "+ event.getMessage().getContentDisplay()
-        ).queue();
+        channel.sendMessageEmbeds(embed).queue();
         
     }
 }
