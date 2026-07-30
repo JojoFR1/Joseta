01af2c6a0effbaa74bea05af62e9361d37595111
Jojo <110781915+JojoFR1@users.noreply.github.com>
2026-07-30 19:07:32 +0200

    add channel delete event

--- src/main/java/dev/jojofr/joseta/events/LogEvent.java
+++ src/main/java/dev/jojofr/joseta/events/LogEvent.java
@@ -7,10 +7,16 @@ import dev.jojofr.joseta.database.Database;
 import dev.jojofr.joseta.database.daos.MessageDao;
 import dev.jojofr.joseta.database.entities.ConfigurationEntity;
 import dev.jojofr.joseta.database.entities.MessageEntity;
+import dev.jojofr.joseta.entities.GuildConfiguration;
 import dev.jojofr.joseta.utils.BotCache;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.events.Event;
+import net.dv8tion.jda.api.events.guild.voice.GuildVoiceUpdateEvent;
+import net.dv8tion.jda.api.events.message.MessageDeleteEvent;
 import net.dv8tion.jda.api.events.message.MessageUpdateEvent;
 
 import java.awt.*;
@@ -21,27 +27,58 @@ public class LogEvent {
     
     @EventHandler(priority = EventPriority.HIGH)
     public void messageUpdateEvent(MessageUpdateEvent event) {
-        ConfigurationEntity configuration = BotCache.getConfiguration(event.getGuild().getIdLong());
-        if (configuration == null || !configuration.moderationLogEnabled) return;
+        if (event.getAuthor().isBot() || event.getAuthor().isSystem()) return;
         
-        TextChannel channel;
-        if (configuration.moderationLogChannelId == null || (channel = event.getGuild().getTextChannelById(configuration.moderationLogChannelId)) == null) return;
+        if (!checkLogEnabled(event.getGuild())) return;
+        GuildConfiguration guildConfig = BotCache.getGuildConfiguration(event.getGuild().getIdLong());
+      
+        MessageEntity oldMessage = Database.withExtension(MessageDao.class, dao -> dao.getById(event.getMessageIdLong()));
+        if (oldMessage == null) return; // Message not found in database, cannot log
+        
+        // Old content is retrieved from database before it gets updated
+       guildConfig.getModerationLogChannel(event.getGuild()).sendMessageEmbeds(
+           buildEmbed(event.getGuild(), event.getAuthor(),
+               Color.ORANGE,
+               "**Message envoyé par <@" + event.getAuthor().getId() + "> modifié (" + event.getMessage().getJumpUrl() + ")**\n\n" +
+                   "**Ancien**\n```" + oldMessage.content + "```\n" +
+                   "**Nouveau**\n```" + event.getMessage().getContentRaw() + "```"
+           )).queue();
+    }
+    
+    @EventHandler(priority = EventPriority.HIGH)
+    public void messageDeleteEvent(MessageDeleteEvent event) {
+        if (!checkLogEnabled(event.getGuild())) return;
+        GuildConfiguration guildConfig = BotCache.getGuildConfiguration(event.getGuild().getIdLong());
         
         MessageEntity oldMessage = Database.withExtension(MessageDao.class, dao -> dao.getById(event.getMessageIdLong()));
         if (oldMessage == null) return; // Message not found in database, cannot log
         
-        MessageEmbed embed = new EmbedBuilder()
-            .setColor(Color.ORANGE)
-            .setAuthor(event.getAuthor().getEffectiveName(), null, event.getAuthor().getEffectiveAvatarUrl())
-            .setDescription("**Message modifié: " + event.getMessage().getJumpUrl() + "**\n\n" +
-                "**Avant**\n```" + oldMessage.content + "```\n" +
-                "**Après**\n```" + event.getMessage().getContentRaw() + "```")
-            .setFooter(event.getGuild().getName(), event.getGuild().getIconUrl())
+        User user = event.getJDA().getUserById(oldMessage.authorId);
+        
+        guildConfig.getModerationLogChannel(event.getGuild()).sendMessageEmbeds(
+            buildEmbed(event.getGuild(), user,
+                Color.RED,
+                "**Message envoyé par <@" + oldMessage.authorId + "> dans <#" + oldMessage.channelId +"> supprimé**\n\n" +
+                "**Contenu**\n```" + oldMessage.content + "```"
+            )).queue();
+    }
+    
+    private MessageEmbed buildEmbed(Guild guild, User user, Color color, String description) {
+        return new EmbedBuilder()
+            .setColor(color)
+            .setAuthor(user != null ? user.getName() : "Utilisateur inconnu", null, user != null ? user.getEffectiveAvatarUrl() : null)
+            .setDescription(description)
+            .setFooter(guild.getName(), guild.getIconUrl())
             .setTimestamp(Instant.now())
             .build();
+    }
+    
+    private boolean checkLogEnabled(Guild guild) {
+        GuildConfiguration guildConfiguration = BotCache.getGuildConfiguration(guild.getIdLong());
+        if (guildConfiguration.configuration == null || !guildConfiguration.configuration.moderationLogEnabled) return false;
         
-        // Old content is retrieved from database before it gets updated
-        channel.sendMessageEmbeds(embed).queue();
+        if (guildConfiguration.getModerationLogChannel(guild) == null) return false;
         
+        return true;
     }
 }
