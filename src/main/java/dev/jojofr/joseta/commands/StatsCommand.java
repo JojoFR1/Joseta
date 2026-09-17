9ada306d64f7ab5073771bd0efbad1cfae8d45b3
Jojo <110781915+JojoFR1@users.noreply.github.com>
2026-09-17 22:39:14 +0200

    use profile color & deferReply

--- src/main/java/dev/jojofr/joseta/commands/StatsCommand.java
+++ src/main/java/dev/jojofr/joseta/commands/StatsCommand.java
@@ -19,41 +19,56 @@ import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 
+import java.awt.*;
+
 @InteractionModule
 public class StatsCommand {
     
     @SlashCommandInteraction(name = "stats", description = "Affiche les statistiques de l'utilisateur.")
     public void stats(SlashCommandInteractionEvent event) {
-        ConfigurationEntity config = BotCache.getConfiguration(event.getGuild().getIdLong());
+        event.deferReply().useComponentsV2().queue(hook -> {
+            long guildId = event.getGuild().getIdLong();
+            long userId = event.getUser().getIdLong();
+//            long botId = event.getJDA().getSelfUser().getIdLong();
+            long botId = 1307015890146955285L;
+            ConfigurationEntity config = BotCache.getConfiguration(guildId);
 
-        Database.useHandle(handle -> {
-            UserEntity dbUser = handle.attach(UserDao.class).getById(event.getUser().getIdLong(), event.getGuild().getIdLong());
+            Database.useHandle(handle -> {
+                UserEntity dbUser = handle.attach(UserDao.class).getById(userId, guildId);
 
-            MessageDao messageDao = handle.attach(MessageDao.class);
-            int messageCount = messageDao.getMemberMessageCount(event.getUser().getIdLong(), event.getGuild().getIdLong());
+                MessageDao messageDao = handle.attach(MessageDao.class);
+                int messageCount = messageDao.getMemberMessageCount(userId, guildId);
 
-            int  countingMessages = 0;
-            int chainBreaks = 0;
-            if (config.countingChannelId != null) {
-                countingMessages = messageDao.getMemberCountingMessageCount(event.getUser().getIdLong(), event.getGuild().getIdLong(), config.countingChannelId, event.getJDA().getSelfUser().getIdLong());
-                chainBreaks = messageDao.getMemberChainBreakCount(event.getUser().getIdLong(), event.getGuild().getIdLong(), config.countingChannelId, event.getJDA().getSelfUser().getIdLong());
-            }
+                int  countingMessages;
+                int chainBreaks;
+                if (config.countingChannelId != null) {
+                    countingMessages = messageDao.getMemberCountingMessageCount(userId, guildId, config.countingChannelId, botId);
+                    chainBreaks = messageDao.getMemberChainBreakCount(userId, guildId, config.countingChannelId, botId);
+                } else {
+                    countingMessages = 0;
+                    chainBreaks = 0;
+                }
 
-            int countingSpecialMessages = 0;
-            int chainBreaksSpecial = 0;
-            if (config.countingSpecialChannelId != null) {
-                countingSpecialMessages = messageDao.getMemberCountingMessageCount(event.getUser().getIdLong(), event.getGuild().getIdLong(), config.countingSpecialChannelId, event.getJDA().getSelfUser().getIdLong());
-                chainBreaksSpecial = messageDao.getMemberChainBreakCount(event.getUser().getIdLong(), event.getGuild().getIdLong(), config.countingSpecialChannelId, event.getJDA().getSelfUser().getIdLong());
-            }
+                int countingSpecialMessages;
+                int chainBreaksSpecial;
+                if (config.countingSpecialChannelId != null) {
+                    countingSpecialMessages = messageDao.getMemberCountingMessageCount(userId, guildId, config.countingSpecialChannelId, botId);
+                    chainBreaksSpecial = messageDao.getMemberChainBreakCount(userId, guildId, config.countingSpecialChannelId, botId);
+                } else {
+                    countingSpecialMessages = 0;
+                    chainBreaksSpecial = 0;
+                }
 
-            Container statsContainer = createStatsContainer(
-                    event.getGuild(), event.getMember(), dbUser, messageCount, countingMessages, chainBreaks, countingSpecialMessages, chainBreaksSpecial);
-
-            event.replyComponents(statsContainer).useComponentsV2().queue();
+                event.getUser().retrieveProfile().queue(profile -> {
+                    Color color = profile.getAccentColor();
+                    Container statsContainer = createStatsContainer(event.getGuild(), event.getMember(), dbUser, messageCount, countingMessages, chainBreaks, countingSpecialMessages, chainBreaksSpecial, color);
+                    hook.editOriginalComponents(statsContainer).useComponentsV2().queue();
+                });
+            });
         });
     }
 
-    private Container createStatsContainer(Guild guild, Member member, UserEntity dbUser, int messageCount, int countingMessages, int chainBreaks, int countingSpecialMessages, int chainBreaksSpecial) {
+    private Container createStatsContainer(Guild guild, Member member, UserEntity dbUser, int messageCount, int countingMessages, int chainBreaks, int countingSpecialMessages, int chainBreaksSpecial, Color color) {
         DiscordTimestamp timestampCreated = DiscordTimestamp.from(member.getTimeCreated());
         DiscordTimestamp timestampJoined = DiscordTimestamp.from(member.getTimeJoined());
         return Container.of(
@@ -97,15 +112,15 @@ public class StatsCommand {
                 ### Comptage
                 **%,d** nombre réussi
                 **%,d** chaînes cassées
-                **%.1f %%** de réussite
+                **%.2f %%** de réussite
                 ### Comptage spécial
                 **%,d** nombre réussi
                 **%,d** chaînes cassées
-                **%.1f %%** de réussite
+                **%.2f %%** de réussite
                 """,
                 countingMessages, chainBreaks, countingMessages == 0 ? 0 : (double) countingMessages / (countingMessages + chainBreaks) * 100,
                 countingSpecialMessages, chainBreaksSpecial, countingSpecialMessages == 0 ? 0 : (double) countingSpecialMessages / (countingSpecialMessages + chainBreaksSpecial) * 100
             )
-        ).withAccentColor(member.getColors().getPrimary());
+        ).withAccentColor(color);
     }
 }
