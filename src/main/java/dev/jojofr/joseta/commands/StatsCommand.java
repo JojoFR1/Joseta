c28de4e8b57f61ca70b8749819d824becad7f6f3
JojoFR1 <110781915+JojoFR1@users.noreply.github.com>
2026-09-17 16:06:40 +0200

    rough draft to better stats

--- src/main/java/dev/jojofr/joseta/commands/StatsCommand.java
+++ src/main/java/dev/jojofr/joseta/commands/StatsCommand.java
@@ -6,8 +6,13 @@ import dev.jojofr.joseta.database.Database;
 import dev.jojofr.joseta.database.daos.MessageDao;
 import dev.jojofr.joseta.database.daos.UserDao;
 import dev.jojofr.joseta.database.entities.UserEntity;
+import dev.jojofr.joseta.utils.DiscordTimestamp;
 import dev.jojofr.joseta.utils.StringUtils;
 import dev.jojofr.joseta.utils.TimeUtils;
+import net.dv8tion.jda.api.components.container.Container;
+import net.dv8tion.jda.api.components.section.Section;
+import net.dv8tion.jda.api.components.textdisplay.TextDisplay;
+import net.dv8tion.jda.api.components.thumbnail.Thumbnail;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 
@@ -19,15 +24,39 @@ public class StatsCommand {
         Database.useHandle(handle -> {
             UserEntity dbUser = handle.attach(UserDao.class).getById(event.getUser().getIdLong(), event.getGuild().getIdLong());
             int messageCount = handle.attach(MessageDao.class).getMemberMessageCount(event.getUser().getIdLong(), event.getGuild().getIdLong());
-            
+
             Member member = event.getMember();
             
-            event.reply("Nombre de messages envoyés : " + StringUtils.formatNumber(messageCount)
-                + "\nTemps passé en vocal : " + (dbUser == null ? "0s" : TimeUtils.formatTime(dbUser.timeVoice / 1000))
-                + "\nNombre de sanctions : " + (dbUser == null ? 0 : dbUser.sanctionCount)
-                + "\nA rejoint le serveur le : <t:" + (member == null ? 0 : member.getTimeJoined().toEpochSecond()) + ":F> (<t:" + (member == null ? 0 : member.getTimeJoined().toEpochSecond()) + ":R>)"
-                + "\nA créé son compte Discord le : <t:" + (member == null ? 0 : member.getTimeCreated().toEpochSecond()) + ":F> (<t:" + (member == null ? 0 : member.getTimeCreated().toEpochSecond()) + ":R>)"
-            ).setEphemeral(true).queue();
+            Container statsContainer = createStatsContainer(member, dbUser, messageCount);
+
+            // event.reply("Nombre de messages envoyés : " + StringUtils.formatNumber(messageCount)
+            //     + "\nTemps passé en vocal : " + (dbUser == null ? "0s" : TimeUtils.formatTime(dbUser.timeVoice / 1000))
+            //     + "\nA rejoint le serveur le : <t:" + (member == null ? 0 : member.getTimeJoined().toEpochSecond()) + ":F> (<t:" + (member == null ? 0 : member.getTimeJoined().toEpochSecond()) + ":R>)"
+            //     + "\nA créé son compte Discord le : <t:" + (member == null ? 0 : member.getTimeCreated().toEpochSecond()) + ":F> (<t:" + (member == null ? 0 : member.getTimeCreated().toEpochSecond()) + ":R>)"
+            // ).setEphemeral(true).queue();
+            
+            event.replyComponents(statsContainer).useComponentsV2().queue();
         });
+    }
+    
+    private Container createStatsContainer(Member member, UserEntity dbUser, int messageCount) {
+        DiscordTimestamp timestampCreated = DiscordTimestamp.from(member.getTimeCreated());
+        DiscordTimestamp timestampJoined = DiscordTimestamp.from(member.getTimeJoined());
+        return Container.of(
+            Section.of(
+                Thumbnail.fromUrl(member.getEffectiveAvatarUrl()),
+                TextDisplay.of("# Statistiques de " + member.getEffectiveName()
+                    + "\n-# À rejoint Discord le " + timestampCreated.longFull() + " (" + timestampCreated.relative() + ")"
+                    + "\n-# _ _                serveur le " + timestampJoined.longFull() + " (" + timestampJoined.relative() + ")")
+            ),
+            
+            TextDisplay.of("Messages envoyés : " + StringUtils.formatNumber(messageCount)
+                + "\n- dont X dans comptage (Y special)"
+                + "\n- dont X cassages de chaînes (Y special)"
+                + "\n- pour un ratio de Z (compte/cassages) et W (comptesp/cassagesp)"
+            ),
+            
+            TextDisplay.of("Temps passé en vocal : " + (dbUser == null ? "0s" : TimeUtils.formatTime(dbUser.timeVoice / 1000)))
+        );
     }
 }
