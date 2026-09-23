a3873eae70fc63e7c78100e6111a8da3e3f6d58e
JojoFR1 <110781915+JojoFR1@users.noreply.github.com>
2026-09-23 11:37:21 +0200

    optimize color average from icon

--- src/main/java/dev/jojofr/joseta/entities/GuildConfiguration.java
+++ src/main/java/dev/jojofr/joseta/entities/GuildConfiguration.java
@@ -1,5 +1,8 @@
 package dev.jojofr.joseta.entities;
 
+import dev.jojofr.joseta.database.Database;
+import dev.jojofr.joseta.database.daos.MessageDao;
+import dev.jojofr.joseta.database.daos.UserDao;
 import dev.jojofr.joseta.database.entities.ConfigurationEntity;
 import dev.jojofr.joseta.utils.Log;
 import net.dv8tion.jda.api.entities.Guild;
@@ -9,6 +12,7 @@ import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.InputStream;
 import java.net.URI;
 import java.util.HashSet;
 import java.util.Set;
@@ -35,28 +39,30 @@ public class GuildConfiguration {
     }
     
     public void updateAccentColor(Guild guild) {
-        if (guild.getIconUrl() == null) { accentColor = null; return; }
+        if (guild.getIconUrl() == null) { accentColor = Color.GRAY; return; }
         
-        try {
-            BufferedImage guildIcon = ImageIO.read(new URI(guild.getIconUrl()).toURL());
-            if (guildIcon == null) { accentColor = null; return; }
+        try (InputStream is = new URI(guild.getIcon().getUrl(64)).toURL().openStream()) {
+            BufferedImage image = ImageIO.read(is);
+            if (image == null) { accentColor = Color.GRAY; return; }
             
-            long red = 0, green = 0, blue = 0, count = 0;
+            int[] pixels = image.getRGB(0, 0, image.getWidth(), image.getHeight(), null, 0, image.getWidth());
+            if (pixels == null || pixels.length == 0) { accentColor = Color.GRAY; return; }
+
+            int pixelCount = pixels.length;
             
-            for (int i = 0; i < guildIcon.getWidth() * guildIcon.getHeight(); i++) {
-                int rgb = guildIcon.getRGB(i % guildIcon.getWidth(), i / guildIcon.getWidth());
-                Color color = new Color(rgb, true);
+            long r = 0, g = 0, b = 0;
+            for (int argb : pixels) {
+                if (((argb >> 24) & 0xFF) < 16) continue;
                 
-                if (color.getAlpha() < 128) continue;
-                red += color.getRed(); green += color.getGreen(); blue += color.getBlue(); count++;
+                r += (argb >> 16) & 0xFF;
+                g += (argb >> 8) & 0xFF;
+                b += argb & 0xFF;
             }
             
-            if (count == 0) { accentColor = null; return; }
-            
-            accentColor = new Color((int) (red / count), (int) (green / count), (int) (blue / count));
+            accentColor = new Color((int) (r / pixelCount), (int) (g / pixelCount), (int) (b / pixelCount));
         } catch (Exception e) {
             Log.err("Failed to update accent color for guild: {} (ID: {})", e, guild.getName(), guild.getIdLong());
-            accentColor = null;
+            accentColor = Color.GRAY;
         }
     }
     
